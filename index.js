a7b000dfc12c58122aee8e5ed0b96447457434ca
Jannis R <mail@jannisr.de>
2016-03-18 23:54:00 +0100

    autocomplete entries

--- index.js
+++ index.js
@@ -30,23 +30,32 @@ const defaults = {
 
 
 
-const renderPrompt = (ctx) =>
-	escapes.eraseLine
-	+ (!ctx.done ? escapes.cursorShow : '')
+const renderPrompt = (ctx) => escapes.eraseLine
 	+ [
-		ctx._.symbol(ctx),
-		ctx._.text,
-		ctx._.delimiter,
+		ctx._.symbol(ctx), ctx._.text, ctx._.delimiter,
 		ctx._.replace(ctx.input)
 	].join(' ')
 
-const render = function (ctx) {
-	let lines = [renderPrompt(ctx)]
-		.concat(ctx.suggestions
-			.map((suggestion) => chalk.yellow(suggestion)))
+var linesRendered = 0
 
-	let linesRendered = lines.length
+const clear = (ctx) => process.stdout.write(
+	escapes.cursorTo(0)
+	+ escapes.cursorMove(0, linesRendered - 1)
+	+ escapes.eraseLines(linesRendered)
+	+ escapes.cursorMove(0, -linesRendered + 1)
+)
+
+var linesRendered = 0
+const render = function (ctx) {
+	clear()
+
+	if (ctx.done) return process.stdout.write(renderPrompt(ctx) + '\n')
+
+	let lines = [renderPrompt(ctx)].concat(ctx.suggestions
+		.map((s, i) => i === ctx.cursor ? chalk.cyan(s) : s))
+	linesRendered = lines.length
 	let lengthOfFistLine = stripAnsi(lines[0]).length
+
 	process.stdout.write(lines.join('\n')
 		+ escapes.cursorTo(0)
 		+ escapes.cursorMove(lengthOfFistLine, -linesRendered + 1))
@@ -56,6 +65,14 @@ const render = function (ctx) {
 
 const onKey = (ctx) => function (input, enc) {
 	input = input.toString(enc)
+
+	if(/\x1b\[+[A-Z]/.test(input)) {
+		let key = input.charAt(input.length - 1);
+		if (key === 'A') return moveUp(ctx)
+		if (key === 'B') return moveDown(ctx)
+		return
+	}
+
 	let code = input.charCodeAt(0); // see https://en.wikipedia.org/wiki/GNU_Readline#Keyboard_shortcuts
 	// if (code === 1)    return first(); // ctrl + A
 	if (code === 3)    return abort(ctx) // ctrl + C
@@ -80,12 +97,24 @@ const onInput = function (ctx, input) {
 
 
 
+const moveUp = function (ctx) {
+	if (ctx.cursor === 0) ctx.cursor = ctx.suggestions.length - 1
+	else ctx.cursor--
+	render(ctx)
+}
+
+const moveDown = function (ctx) {
+	ctx.cursor = ++ctx.cursor % ctx.suggestions.length
+	render(ctx)
+}
+
+
+
 const abort = function (ctx) {
 	process.stdin.destroy()
 	ctx.aborted = ctx.done = true
 	render(ctx)
 
-	process.stdout.write(escapes.cursorHide)
 	ctx.reject(null)
 }
 
@@ -94,12 +123,11 @@ const submit = function (ctx) {
 	ctx.done = true
 	render(ctx)
 
-	process.stdout.write(escapes.cursorHide)
-	ctx.resolve(ctx.input)
+	ctx.resolve(ctx.suggestions[ctx.cursor])
 }
 
 const remove = function (ctx) {
-	if (ctx.input.length === 0) process.stdout.write(escapes.beep)
+	if (ctx.input.length === 0) return process.stdout.write(escapes.beep)
 	ctx.input = ctx.input.slice(0, -1)
 	ctx.suggestions = ctx._.suggest(ctx.input)
 	render(ctx)
@@ -116,7 +144,8 @@ const prompt = function (text, opt) {
 		done:        false,
 		aborted:     false,
 		input:       '',
-		suggestions: []
+		suggestions: [],
+		cursor:      0
 	}
 
 	process.stdin.setRawMode(true)
