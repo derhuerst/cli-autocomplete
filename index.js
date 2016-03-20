f213627f0921ac18cad367c07a0d6ed4f2dfe2e3
Jannis R <mail@jannisr.de>
2016-03-20 02:45:49 +0100

    rewrite in prototypal style

--- index.js
+++ index.js
@@ -8,151 +8,149 @@ const ui =        require('./ui')
 
 
 
-const renderPrompt = (ctx) => [
-	ui.symbol(ctx.done, ctx.aborted),
-	ctx._.text,
-	ui.delimiter,
-	(ctx.done && ctx.cursor in ctx.suggestions
-		? ctx.suggestions[ctx.cursor].title
-		: ctx.render(ctx.input))
-].join(' ')
+const Prompt = {
 
-var linesRendered = 0
+	renderPrompt: function () { return [
+		ui.symbol(this.done, this.aborted), this.text, ui.delimiter,
+		this.done && this.cursor in this.suggestions
+			? this.suggestions[this.cursor].title
+			: this.transform(this.input)
+	].join(' ') }
 
-const clear = (ctx) => // cursor is at the end of the first rendered line
-	(linesRendered > 0 ? escapes.cursorDown(linesRendered) : '') // move to last rendered line
-	+ escapes.eraseLines(linesRendered + 1) // move to first rendered line, deleting everything
+	, renderSuggestion: (cursor) =>
+		(s, i) => i === cursor ? chalk.cyan(s.title) : s.title
 
-const renderSuggestion = (ctx) =>
-	(s, i) => i === ctx.cursor ? chalk.cyan(s.title) : s.title
+	, linesRendered: 0
 
-const render = function (ctx) {
-	process.stdout.write(clear())
-
-	if (ctx.done) {
-		process.stdout.write(renderPrompt(ctx) + '\n')
-		linesRendered = 2
-	} else {
-		let lines = [renderPrompt(ctx)]
-			.concat(ctx.suggestions.map(renderSuggestion(ctx)))
-		linesRendered = lines.length
-
-		process.stdout.write(
-			lines.join('\n')
-			+ (lines.length > 1 ? escapes.cursorUp(lines.length - 1) : '')
-			+ escapes.cursorTo(stripAnsi(lines[0]).length)
-		)
-	}
-}
-
-
-
-const onKey = (ctx) => function (input, enc) {
-	input = input.toString(enc)
-
-	if(/\x1b\[+[A-Z]/.test(input)) {
-		let key = input.charAt(input.length - 1);
-		if (key === 'A') return moveUp(ctx)
-		if (key === 'B') return moveDown(ctx)
-		return
+	, clear: function () { return ''
+		// move to last rendered line
+		+ (this.linesRendered > 0 ? escapes.cursorDown(this.linesRendered) : '')
+		// move to first rendered line, deleting everything
+		+ escapes.eraseLines(this.linesRendered + 1)
 	}
 
-	let code = input.charCodeAt(0); // see https://en.wikipedia.org/wiki/GNU_Readline#Keyboard_shortcuts
-	// if (code === 1)    return first(); // ctrl + A
-	if (code === 3)    return abort(ctx) // ctrl + C
-	if (code === 13)   return submit(ctx) // return key
-	if (code === 127)  return remove(ctx) // backspace
-	// if (code === 4)    return process.exit(); // ctrl + D
-	// if (code === 5)    return last(); // ctrl + E
-	// if (code === 7)    return reset(); // ctrl + G
-	// if (code === 9)    return right(); // ctrl + I / tab
-	// if (code === 10)   return submit(); // ctrl + J
-	// if (code === 27)   return abort(); // escape
-	// if (code === 8747) return left(); // alt + B
-	// if (code === 402)  return right(); // alt + F
-	onInput(ctx, input)
-}
+	, render: function () {
+		process.stdout.write(this.clear())
 
-const onInput = function (ctx, input) {
-	ctx.input += input
-	ctx.suggestions = ctx._.suggest(ctx.input)
-	render(ctx)
-}
+		if (this.done) {
+			process.stdout.write(this.renderPrompt() + '\n')
+			this.linesRendered = 2
+		} else {
+			let lines = [this.renderPrompt()]
+				.concat(this.suggestions.map(this.renderSuggestion(this.cursor)))
+			this.linesRendered = lines.length
+
+			process.stdout.write(
+				lines.join('\n')
+				+ (lines.length > 1 ? escapes.cursorUp(lines.length - 1) : '')
+				+ escapes.cursorTo(stripAnsi(lines[0]).length))
+				// cursor is at the end of the first rendered line now
+		}
+	}
 
 
 
-const moveUp = function (ctx) {
-	if (ctx.cursor === 0) ctx.cursor = ctx.suggestions.length - 1
-	else ctx.cursor--
-	render(ctx)
-}
+	, onKey: function (input, enc) {
+		input = input.toString(enc)
+		let key = input.charAt(input.length - 1)
+		// see https://en.wikipedia.org/wiki/GNU_Readline#Keyboard_shortcuts
+		let code = input.charCodeAt(0)
 
-const moveDown = function (ctx) {
-	ctx.cursor = ++ctx.cursor % ctx.suggestions.length
-	render(ctx)
-}
+		if(/\x1b\[+[A-Z]/.test(input)) {
+			     if (key === 'A') this.up()
+			else if (key === 'B') this.down()
+			return
+		}
+
+		     // if (code === 1)    first(); // ctrl + A
+		     if (code === 3)    this.abort()  // ctrl + C
+		else if (code === 13)   this.submit() // return key
+		else if (code === 127)  this.delete() // backspace
+		// else if (code === 4)    process.exit(); // ctrl + D
+		// else if (code === 5)    last(); // ctrl + E
+		// else if (code === 7)    reset(); // ctrl + G
+		// else if (code === 9)    right(); // ctrl + I / tab
+		// else if (code === 10)   submit(); // ctrl + J
+		// else if (code === 27)   abort(); // escape
+		else {
+			this.input += input
+			this.suggestions = this.suggest(this.input)
+			this.render()
+		}
+	}
+
+	, up: function () {
+		if (this.cursor === 0) this.cursor = this.suggestions.length - 1
+		else this.cursor--
+		this.render()
+	}
+
+	, down: function () {
+		this.cursor = ++this.cursor % this.suggestions.length
+		this.render()
+	}
+
+	, abort: function () {
+		process.stdin.destroy()
+		this.aborted = this.done = true
+		this.render()
+		this.reject(null)
+	}
+
+	, submit: function () {
+		process.stdin.destroy()
+		this.done = true
+		this.render()
+
+		if (this.cursor in this.suggestions)
+			this.resolve(this.suggestions[this.cursor].value)
+		else this.resolve(null)
+	}
+
+	, delete: function () {
+		if (this.input.length === 0)
+			return process.stdout.write(escapes.beep)
+
+		this.input = this.input.slice(0, -1)
+		this.suggestions = this.suggest(this.input)
+		this.render()
+	}
 
 
 
-const abort = function (ctx) {
-	process.stdin.destroy()
-	ctx.aborted = ctx.done = true
-	render(ctx)
+	, transform: ui.render()
+	, suggest:   () => []
+	, done:      false
+	, aborted:   false
+	, input:     ''
+	, cursor:    0
+	, resolve:   () => null
+	, reject:    () => null
 
-	ctx.reject(null)
-}
-
-const submit = function (ctx) {
-	process.stdin.destroy()
-	ctx.done = true
-	render(ctx)
-
-	if (ctx.cursor in ctx.suggestions)
-		ctx.resolve(ctx.suggestions[ctx.cursor].value)
-	else ctx.resolve(null)
-}
-
-const remove = function (ctx) {
-	if (ctx.input.length === 0) return process.stdout.write(escapes.beep)
-	ctx.input = ctx.input.slice(0, -1)
-	ctx.suggestions = ctx._.suggest(ctx.input)
-	render(ctx)
-}
-
-
-
-const defaults = {
-	type:    'default',
-	text:    '',
-	suggest: (input) => []
 }
 
 const prompt = function (text, opt) {
-	if ('string' !== typeof text) throw new Error('text must be a string.')
-	if (arguments.length < 2 || 'object' !== typeof opt) opt = {}
-	opt = Object.assign({}, defaults, opt, {text})
+	let instance = Object.create(Prompt)
 
-	let ctx = {
-		_:           opt,
-		done:        false,
-		aborted:     false,
-		input:       '',
-		suggestions: [],
-		cursor:      0,
-		render:      ui.render(opt.type)
-	}
+	if ('string' !== typeof text) throw new Error('text must be a string.')
+	instance.text = text
+
+	if (arguments.length < 2 || 'object' !== typeof opt) opt = {}
+	if ('string' === typeof opt.type) instance.transform = ui.render(opt.type)
+	if ('function' === typeof opt.suggest) instance.suggest = opt.suggest
 
 	process.stdin.setRawMode(true)
-	process.stdin.on('data', onKey(ctx))
+	process.stdin.on('data', instance.onKey.bind(instance))
 	// todo: on 'end'
-	onInput(ctx, ctx.input)
+	instance.suggestions = instance.suggest(instance.input)
+	instance.render()
 
 	return new Promise(function (resolve, reject) {
-		ctx.resolve = resolve
-		ctx.reject =  reject
+		instance.resolve = resolve
+		instance.reject =  reject
 	})
 }
 
 
 
-module.exports = Object.assign(prompt, {defaults})
+module.exports = Object.assign(prompt, Prompt)
